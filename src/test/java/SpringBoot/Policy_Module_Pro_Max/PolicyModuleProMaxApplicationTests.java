fe2f9e8ef8566e080875650a5ede8778b0e49e66
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2021-05-31 22:44:18 +0530

    Testing Folder

---
+++ src/test/java/SpringBoot/Policy_Module_Pro_Max/PolicyModuleProMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot.Policy_Module_Pro_Max;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyModuleProMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
